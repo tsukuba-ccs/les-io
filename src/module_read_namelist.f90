59e2c9c37d4e82e7b6eb1812a7865fe1a871dd86
Kohei Sugihara <sugihara@hpcs.cs.tsukuba.ac.jp>
2019-11-05 17:25:30 +0900

    initial commit for release

---
+++ src/module_read_namelist.f90
@@ -0,0 +1,19 @@
+! Copyright (C) 2015-2019 by LES-IO authors.
+! See AUTHORS and LICENSE in top-level directory.
+module module_read_namelist
+	contains
+	
+	subroutine ReadNamelist()
+		use module_param_namelist
+		use module_mpi
+		implicit none
+		
+		open( 99, file='namelist.input',status='unknown')
+		read( 99, nml=parallel )
+		read( 99, nml=domain )	
+		read( 99, nml=physics )
+		close( 99 )
+
+		return	
+	end subroutine ReadNamelist
+end module module_read_namelist
